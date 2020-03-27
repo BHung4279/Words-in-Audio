7fb27ced4ed0768dc572f146d14f2bed81809ad0
Ben Hung <kopnosema@gmail.com>
2020-03-27 11:36:48 -0500

    basically finished

--- wavtotext.py
+++ wavtotext.py
@@ -3,10 +3,12 @@ from os import path
 from pydub import AudioSegment
 
 filename = "test.wav"
-
+file = open("text.txt", "w+")
 r = sr.Recognizer()
 
 with sr.AudioFile(filename) as source:
     audio_data = r.record(source)
     text = r.recognize_google(audio_data)
-    print(text)
+    file.write(text)
+
+file.close()
